097364fc1b5e1ec1a142e6ef4ec6c6a64d4cce7d
Ross Arnold <rossarn@gmail.com>
2025-08-27 12:32:16 +0100

    Add Spring MVC skeleton generated from https://start.spring.io/

---
+++ src/main/java/com/example/rossarn_at_gmail_dot_com/shopping_list/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.rossarn_at_gmail_dot_com.shopping_list;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ShoppingListApplication.class);
+	}
+
+}
---
+++ src/test/java/com/example/rossarn_at_gmail_dot_com/shopping_list/ShoppingListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rossarn_at_gmail_dot_com.shopping_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
